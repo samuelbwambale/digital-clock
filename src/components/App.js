1caa427bb4c11fe220bd81ca97f50bf6e5f63a2d
samuelbwambale <simplesam63@gmail.com>
2022-03-03 09:17:27 -0600

    add footer to clock

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import Title from './Title';
 import Clock from './Clock';
+import Footer from './Footer';
+
 class App extends Component{
     render(){
         return(  
         <div>
             <Title title="Digital Clock"></Title>
             <Clock/>
+            <Footer footer = "@samuelbwambale"/>
+
         </div>
         )
         
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <h4>{this.props.footer}</h4>
+        )
+    }
+}
+
+export default Footer;
